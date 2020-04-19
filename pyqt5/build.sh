7b7fca74d244743f4c5e1bea013ecc043a313509
regiscely <regiscel@fr.ibm.com>
2020-04-19 14:58:10 +0000

    update

--- pyqt5/build.sh
+++ pyqt5/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Specific environement build
+# Modify LD_LIBRARY_PATH QMAKE_PATH according to your qt5.14.1 installation
+# Maybe module load is sufficient
+
 export LD_LIBRARY_PATH=/usr/local/lib64:$LD_LIBRARY_PATH
+export QMAKE_PATH=/usr/local/Qt-5.14.1/bin/qmake
 
 if [ `uname` == Darwin ]; then
     export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
--- pyqtwebengine/build.sh
+++ pyqtwebengine/build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 
-
+# To be adapted to your local environment
 export LD_LIBRARY_PATH=/usr/local/lib64:/usr/local/Qt-5.14.1/lib:$LD_LIBRARY_PATH
 
-echo "REEEEEEEEEEEEEEEEEEEEEGGGGGGGGGGGGGGIIIIIIIIISSSSSSSSSSSSS"
-python -c "import PyQt5.QtCore"
 
 
 if [ `uname` == Darwin ]; then
@@ -18,9 +16,9 @@ if [ `uname` == Linux ]; then
 fi
 
 
-$PYTHON configure.py --verbose
-#        --verbose \
-#        -q /usr/local/Qt-5.14.1/bin/qmake
+$PYTHON configure.py \
+        --verbose \
+        -q /usr/local/Qt-5.14.1/bin/qmake
 
 make -j $MAKE_JOBS
 make install
